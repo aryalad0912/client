5ccc25d01369e09597fdde9c4ee94905c6463270
Elnur Farzaliyev <farzaliyevelnur00@gmail.com>
2022-03-06 17:04:48 +0400

    client-auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router,Routes,Route,Link } from 'react-router-dom';
 import './App.css';
+import Landing from './pages/Landing';
+import Register from './pages/Register';
+import Error from './pages/Error';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Dashboard/>}/>
+            <Route path='/landing' element={<Landing/>}/>
+            <Route path='/register' element={<Register/>}/>
+            <Route path='*' element={<Error/>}/>
+          </Routes>
+        </Router>
+    </div>  
   );
 }
 
